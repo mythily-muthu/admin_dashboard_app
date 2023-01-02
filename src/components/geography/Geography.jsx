508a57ac162e2acf5b61cac66ccfe7de97ba71b7
mythily-muthu <mythilimuthu18@gmail.com>
2023-01-02 23:08:09 +0530

    Feat:  Border radius added for the geography chart

--- src/components/geography/Geography.jsx
+++ src/components/geography/Geography.jsx
@@ -6,7 +6,11 @@ const Geography = () => {
   return (
     <Box m="20px">
       <Header title="Geography Chart" subtitle="Simple Geography Chart" />
-      <Box height="75vh">
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
         <GeographyChart />
       </Box>
     </Box>
